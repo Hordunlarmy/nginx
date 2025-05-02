d79e6253913b794e0897b3cfc6c6c65a2d0b76ef
Hordunlarmy <Hordunlarmy@gmail.com>
2025-05-02 12:42:28 +0100

    style(nginx): Remove quotes around upgrade in proxy_set_header

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,7 +78,7 @@ generate_location_blocks() {
       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
       proxy_set_header X-Forwarded-Proto \$scheme;
       proxy_set_header Upgrade \$http_upgrade;
-      proxy_set_header Connection 'upgrade';
+      proxy_set_header Connection upgrade;
       proxy_buffering off;"
 			;;
 		php)
